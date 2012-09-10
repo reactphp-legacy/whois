8c5c32cc4e9c47f7f0692cd4b02359a33bee73ea
Sasa Stamenkovic <umpirsky@gmail.com>
2012-09-10 23:35:38 +0200

    Removed unused use statements.

--- tests/React/Whois/Stub/ConnectionStub.php
+++ tests/React/Whois/Stub/ConnectionStub.php
@@ -4,7 +4,6 @@ namespace React\Whois\Stub;
 
 use Evenement\EventEmitter;
 use React\Socket\ConnectionInterface;
-use React\Stream\WritableStreamInterface;
 
 abstract class ConnectionStub extends EventEmitter implements ConnectionInterface
 {
